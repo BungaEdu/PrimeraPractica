676ef02b942108f79a798e18eca7a3d75697d459
AlumnoM <eduardoflores.9595@gmail.com>
2022-09-15 13:04:10 +0200

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    //var es la variables
+    //val es una constante
+    //si declaras la variables, siempre va a tener que ser así
+    //pero si no la declaras, luego la puedes modificar
+    
+}
